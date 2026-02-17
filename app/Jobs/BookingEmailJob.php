9e9706df1e4fbb6f0e69f190127c9ab67c2a2a3f
Ali Rezvani Ara <ali.rezvaniara@gmail.com>
2026-02-18 01:13:59 +0330

    Queue job email

--- app/Jobs/BookingEmailJob.php
+++ app/Jobs/BookingEmailJob.php
@@ -2,26 +2,52 @@
 namespace App\Jobs;
 
 use App\Models\Booking;
+use App\Models\Hotel;
+use App\Models\Room;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class BookingEmailJob implements ShouldQueue
 {
     use InteractsWithQueue, Queueable, SerializesModels,Dispatchable;
 
     public Booking $reservationModel;
+    public Hotel $hotel;
+    public Room $room;
 
     public function __construct(Booking $reservation)
     {
         $this->reservationModel = $reservation;
+        $this->room = Room::find($reservation->room_id);
+        $this->hotel = Hotel::find($this->room->hotel_id);
+
+
     }
 
     public function handle(): void
     {
-        //send email here
+//        dd([$this->reservationModel,$this->room,$this->hotel]);
+        $mail = new PHPMailer(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->Encoding = 'base64';
+        $mail->isSMTP();
+        $mail->Host = env("MAIL_HOST");
+        $mail->SMTPAuth = true;
+        $mail->Username = env("MAIL_USERNAME");
+        $mail->Password = env("MAIL_PASSWORD");
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = env("MAIL_PORT");
+        $mail->setFrom(env("MAIL_USERNAME"), "Damoon Travel");
+        $mail->addReplyTo($this->reservationModel->user_email);
+        $mail->addAddress($this->reservationModel->user_email, mb_encode_mimeheader($this->reservationModel->user_name));
+        $mail->isHTML(true);
+        $mail->Subject = 'Book Info';
+        $mail->Body = view("mail.bookInfo")->with("book", $this->reservationModel)->with("room", $this->room)->with("hotel", $this->hotel);
+        $mail->AltBody = "successfully booked";
+        $mail->send();
     }
 }
---
+++ resources/views/mail/bookInfo.blade.php
@@ -0,0 +1,2 @@
+all data about book and room and hotel are available using {{$book}} {{$room}} {{$hotel}}
+this page can design in the future
